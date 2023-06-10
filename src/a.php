dbf4b6fc0c06e1f8e442dd2af295b06f4ff2be79
Nugroho Indra Kurniawan <nugrohoindra18@gmail.com>
2023-06-10 15:07:22 +0700

    menambahkan signup

---
+++ src/a.php
@@ -0,0 +1,10 @@
+<div class="username">
+                    <div class="namaDepan">
+                        <label for="exampleInputFName" class="form-label">Nama Depan</label><br>
+                        <input type="username" class="form-control" id="exampleInputFName" aria-describedby="username" placeholder="Masukan nama depan">
+                    </div>
+                    <div class="namaBelakang">
+                        <label for="exampleInputBName" class="form-label">Nama Belakng</label><br>
+                        <input type="username" class="form-control" id="exampleInputBName" aria-describedby="username" placeholder="Masukan nama belakang">
+                    </div>
+                </div>
---
+++ src/signup.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/signup.css">
+    <title>Sistem Informasi Pendistribusian Logistik Pasca Tsunami | Sign up</title>
+</head>
+<body>
+    <div class="signin">
+        <div class="to-signup">
+            <div class="signup-lay2">
+                <label>Apakah udah memiliki Akun ?</label><br>
+                <a class="signup-btn" href="signin.php">Masuk</a>
+            </div>
+        </div>
+        <div class="signin2">
+            <form>
+                <div class="logo-">
+                    <img src="../icon/Graphic_Elements.png" alt="">
+                </div>
+
+                <div class="username">
+                    <div class="username">
+                        <div class="namaDepan">
+                            <label for="exampleInputFName" class="form-label">Nama Depan</label><br>
+                            <input type="username" class="form-control" id="exampleInputFName" aria-describedby="username" placeholder="Masukan nama depan">
+                        </div>
+                        <div class="namaBelakang">
+                            <label for="exampleInputBName" class="form-label">Nama Belakang</label><br>
+                            <input type="username" class="form-control" id="exampleInputBName" aria-describedby="username" placeholder="Masukan nama belakang">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="email">
+                    <label for="exampleInputEmail1" class="form-label">Alamat Email</label><br>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan email">
+                </div>
+
+                <div class="password-create">
+                    <label for="exampleInputPassword1" class="form-label">Buat Kata Sandi</label><br>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Buat kata sandi">
+                </div>
+
+                <div class="password">
+                    <label for="exampleInputPassword2" class="form-label">Konfirmasi Kata Sandi</label><br>
+                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Konfirmasi kata sandi">
+                </div>
+
+                <div class="btn-form">
+                    <button type="submit" class="btn btn-primary">Daftar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
